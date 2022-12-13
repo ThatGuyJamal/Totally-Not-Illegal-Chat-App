b7315aa91468d40714d65aa4ca6dc31f95019612
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-12-13 14:53:20 -0500

    Update PrefixHandler.ts

--- lib/command-handler/PrefixHandler.ts
+++ lib/command-handler/PrefixHandler.ts
@@ -2,61 +2,61 @@ import guildPrefixSchema from "../models/guild-prefix-schema";
 import WOK from "../../typings";
 
 class PrefixHandler {
-  // <guildId: prefix>
-  private _prefixes = new Map();
-  private _defaultPrefix = "!";
-  private _instance: WOK;
+	// <guildId: prefix>
+	private _prefixes = new Map();
+	private _defaultPrefix = "a!";
+	private _instance: WOK;
 
-  constructor(instance: WOK) {
-    this._instance = instance;
+	constructor(instance: WOK) {
+		this._instance = instance;
 
-    this.loadPrefixes();
-  }
+		this.loadPrefixes();
+	}
 
-  private async loadPrefixes() {
-    if (!this._instance.isConnectedToDB) {
-      return;
-    }
+	private async loadPrefixes() {
+		if (!this._instance.isConnectedToDB) {
+			return;
+		}
 
-    const results = await guildPrefixSchema.find({});
+		const results = await guildPrefixSchema.find({});
 
-    for (const result of results) {
-      this._prefixes.set(result._id, result.prefix);
-    }
-  }
+		for (const result of results) {
+			this._prefixes.set(result._id, result.prefix);
+		}
+	}
 
-  public get defaultPrefix() {
-    return this._defaultPrefix;
-  }
+	public get defaultPrefix() {
+		return this._defaultPrefix;
+	}
 
-  public get(guildId?: string) {
-    if (!guildId) {
-      return this.defaultPrefix;
-    }
+	public get(guildId?: string) {
+		if (!guildId) {
+			return this.defaultPrefix;
+		}
 
-    return this._prefixes.get(guildId) || this.defaultPrefix;
-  }
+		return this._prefixes.get(guildId) || this.defaultPrefix;
+	}
 
-  public async set(guildId: string, prefix: string) {
-    if (!this._instance.isConnectedToDB) {
-      return;
-    }
+	public async set(guildId: string, prefix: string) {
+		if (!this._instance.isConnectedToDB) {
+			return;
+		}
 
-    this._prefixes.set(guildId, prefix);
+		this._prefixes.set(guildId, prefix);
 
-    await guildPrefixSchema.findOneAndUpdate(
-      {
-        _id: guildId,
-      },
-      {
-        _id: guildId,
-        prefix,
-      },
-      {
-        upsert: true,
-      }
-    );
-  }
+		await guildPrefixSchema.findOneAndUpdate(
+			{
+				_id: guildId,
+			},
+			{
+				_id: guildId,
+				prefix,
+			},
+			{
+				upsert: true,
+			}
+		);
+	}
 }
 
 export default PrefixHandler;
