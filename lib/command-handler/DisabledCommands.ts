d3a06664bad8ad7a667d06e44c21cd0473404e43
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-12-13 15:54:15 -0500

    updates

--- lib/command-handler/DisabledCommands.ts
+++ lib/command-handler/DisabledCommands.ts
@@ -2,63 +2,63 @@ import disabledCommandSchema from "../models/disabled-commands-schema";
 import WOK from "../../typings";
 
 class DisabledCommands {
-  // array of `${guildId}-${commandName}`
-  private _disabledCommands: string[] = [];
-  private _instance: WOK;
+	// array of `${guildId}-${commandName}`
+	private _disabledCommands: string[] = [];
+	private _instance: WOK;
 
-  constructor(instance: WOK) {
-    this._instance = instance;
+	constructor(instance: WOK) {
+		this._instance = instance;
 
-    this.loadDisabledCommands();
-  }
+		this.loadDisabledCommands();
+	}
 
-  async loadDisabledCommands() {
-    if (!this._instance.isConnectedToDB) {
-      return;
-    }
+	async loadDisabledCommands() {
+		if (!this._instance.isConnectedToDB) {
+			return;
+		}
 
-    const results = await disabledCommandSchema.find({});
+		const results = await disabledCommandSchema.find({});
 
-    for (const result of results) {
-      this._disabledCommands.push(result._id);
-    }
-  }
+		for (const result of results) {
+			this._disabledCommands.push(result._id);
+		}
+	}
 
-  async disable(guildId: string, commandName: string) {
-    if (
-      !this._instance.isConnectedToDB ||
-      this.isDisabled(guildId, commandName)
-    ) {
-      return;
-    }
+	async disable(guildId: string, commandName: string) {
+		if (
+			!this._instance.isConnectedToDB ||
+			this.isDisabled(guildId, commandName)
+		) {
+			return;
+		}
 
-    const _id = `${guildId}-${commandName}`;
-    this._disabledCommands.push(_id);
+		const _id = `${guildId}-${commandName}`;
+		this._disabledCommands.push(_id);
 
-    try {
-      await new disabledCommandSchema({
-        _id,
-      }).save();
-    } catch (ignored) {}
-  }
+		try {
+			await new disabledCommandSchema({
+				_id,
+			}).save();
+		} catch (ignored) {}
+	}
 
-  async enable(guildId: string, commandName: string) {
-    if (
-      !this._instance.isConnectedToDB ||
-      !this.isDisabled(guildId, commandName)
-    ) {
-      return;
-    }
+	async enable(guildId: string, commandName: string) {
+		if (
+			!this._instance.isConnectedToDB ||
+			!this.isDisabled(guildId, commandName)
+		) {
+			return;
+		}
 
-    const _id = `${guildId}-${commandName}`;
-    this._disabledCommands = this._disabledCommands.filter((id) => id !== _id);
+		const _id = `${guildId}-${commandName}`;
+		this._disabledCommands = this._disabledCommands.filter((id) => id !== _id);
 
-    await disabledCommandSchema.deleteOne({ _id });
-  }
+		await disabledCommandSchema.deleteOne({ _id });
+	}
 
-  isDisabled(guildId: string, commandName: string) {
-    return this._disabledCommands.includes(`${guildId}-${commandName}`);
-  }
+	isDisabled(guildId: string, commandName: string) {
+		return this._disabledCommands.includes(`${guildId}-${commandName}`);
+	}
 }
 
 export default DisabledCommands;
--- lib/command-handler/commands/togglecommand.ts
+++ lib/command-handler/commands/togglecommand.ts
@@ -5,54 +5,63 @@ import CommandType from "../../util/CommandType";
 import { CommandUsage } from "../../../typings";
 
 export default {
-  description: "Toggles a command on or off for your guild",
+	description: "Toggles a command on or off for your guild",
 
-  type: CommandType.SLASH,
-  guildOnly: true,
+	type: CommandType.SLASH,
+	guildOnly: true,
 
-  permissions: [PermissionFlagsBits.Administrator],
+	permissions: [PermissionFlagsBits.Administrator],
 
-  options: [
-    {
-      name: "command",
-      description: "The command to toggle on or off",
-      type: ApplicationCommandOptionType.String,
-      required: true,
-      autocomplete: true,
-    },
-  ],
+	options: [
+		{
+			name: "command",
+			description: "The command to toggle on or off",
+			type: ApplicationCommandOptionType.String,
+			required: true,
+			autocomplete: true,
+		},
+	],
 
-  autocomplete: (command: Command) => {
-    return [...command.instance.commandHandler.commands.keys()];
-  },
+	autocomplete: (command: Command) => {
+		return [...command.instance.commandHandler.commands.keys()];
+	},
 
-  callback: async (commandUsage: CommandUsage) => {
-    const { instance, guild, text: commandName, interaction } = commandUsage;
+	callback: async (commandUsage: CommandUsage) => {
+		const { instance, guild, text: commandName, interaction } = commandUsage;
 
-    if (!instance.isConnectedToDB) {
-      return {
-        content:
-          "This bot is not connected to a database which is required for this command. Please contact the bot owner.",
-        ephemeral: true,
-      };
-    }
+		if (!instance.isConnectedToDB) {
+			return {
+				content:
+					"This bot is not connected to a database which is required for this command. Please contact the bot owner.",
+				ephemeral: true,
+			};
+		}
 
-    const { disabledCommands } = instance.commandHandler;
+		// Check if the command to disable is the same as this command
 
-    if (disabledCommands.isDisabled(guild!.id, commandName)) {
-      await disabledCommands.enable(guild!.id, commandName);
+		if (commandName === "togglecommand") {
+			return {
+				content: "You cannot disable this command!",
+				ephemeral: true,
+			};
+		}
 
-      return {
-        content: `Command "${commandName}" has been enabled`,
-        ephemeral: true,
-      };
-    } else {
-      await disabledCommands.disable(guild!.id, commandName);
+		const { disabledCommands } = instance.commandHandler;
 
-      return {
-        content: `Command "${commandName}" has been disabled`,
-        ephemeral: true,
-      };
-    }
-  },
+		if (disabledCommands.isDisabled(guild!.id, commandName)) {
+			await disabledCommands.enable(guild!.id, commandName);
+
+			return {
+				content: `Command "${commandName}" has been enabled`,
+				ephemeral: true,
+			};
+		} else {
+			await disabledCommands.disable(guild!.id, commandName);
+
+			return {
+				content: `Command "${commandName}" has been disabled`,
+				ephemeral: true,
+			};
+		}
+	},
 };
