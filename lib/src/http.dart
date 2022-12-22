caf475913226e7f21c4158f1ed63eef0f25b15dc
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-12-21 23:44:32 -0500

    fixes and improvements

--- lib/src/http.dart
+++ lib/src/http.dart
@@ -10,16 +10,6 @@ import 'package:chat_app/src/settings.dart';
 import 'package:chat_app/src/utils.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
-FutureOr errorHandler(HttpRequest req, HttpResponse res) {
-  res.statusCode = 500;
-  return {'message': 'Internal server error'};
-}
-
-FutureOr missingHandler(HttpRequest req, HttpResponse res) {
-  res.statusCode = 404;
-  return {'message': 'This route does not exist'};
-}
-
 // Starts a server on port 8080
 Future<void> serveHttpServer() async {
   // Create the Alfred instance
@@ -43,7 +33,7 @@ Future<void> serveHttpServer() async {
   // Path to the html code file
   // https://api.dart.dev/stable/2.18.6/dart-io/Platform/script.html
   String htmlFilePath =
-      Platform.script.resolve('./src/chat-client.html').toFilePath();
+      Platform.script.resolve('./public/chat-client.html').toFilePath();
 
   // Deliver web client for chat
   app.get('/', (req, res) => File(htmlFilePath));
@@ -67,7 +57,7 @@ Future<void> serveHttpServer() async {
       },
       onClose: (ws) async {
         users.forEach((user) async {
-          String id = randomizeString(user.hashCode.toString(), randomUserId);
+          String id = mixTwoStrings(user.hashCode.toString(), randomUserId);
 
           user.send('A user has left.');
           await messages
@@ -83,10 +73,10 @@ Future<void> serveHttpServer() async {
       },
       onMessage: (ws, dynamic data) async {
         users.forEach((user) async {
-          String id = randomizeString(user.hashCode.toString(), randomUserId);
-
           // Send the data to the client
           user.send(data);
+
+          String id = mixTwoStrings(user.hashCode.toString(), randomUserId);
 
           // Save the message to the database
           await messages
@@ -100,6 +90,35 @@ Future<void> serveHttpServer() async {
                   value.document != null ? print(value.document) : null)
               .catchError((error) =>
                   print("Error inserting message to database: $error"));
+
+          // Handle admin commands
+
+          String adminCommandPrefix = 'admin::';
+          String adminCommandSuffix = '::';
+
+          // Check if the message data is a command
+          if (data is String && data.startsWith(adminCommandPrefix)) {
+            // Get the command
+            String command = data.split(adminCommandSuffix)[1];
+
+            // Check if the command is valid
+            if (adminCommands.contains(command)) {
+              // Get the total number of messages
+              if (command == 'total-messages-user') {
+                await messages
+                    .count(where.eq('userHashId', id))
+                    .then(
+                        (value) => user.send('Total messages you sent: $value'))
+                    .catchError((error) => print(
+                        "Error getting total messages from database: $error"));
+              }
+            }
+
+            // If the command is not valid
+            else {
+              user.send('Invalid command!');
+            }
+          }
         });
       },
     );
--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,12 +1,18 @@
+import 'dart:async';
+
 import 'package:xid/xid.dart';
+import 'package:alfred/alfred.dart';
+
+// A list of admin commands
+final List<String> adminCommands = ["total-messages-user"];
 
 // Creates a random Id
 Xid generateId() {
   return Xid();
 }
 
-// Creates a random string from old strings
-String randomizeString(String str1, String str2) {
+/// Returns a new string with the characters from both strings mixed together.
+String mixTwoStrings(String str1, String str2) {
   String newString = '';
   if (str1.length > str2.length) {
     for (int i = 0; i < str1.length; i++) {
@@ -26,4 +32,16 @@ String randomizeString(String str1, String str2) {
     }
   }
   return newString;
+}
+
+// Error handler
+FutureOr errorHandler(HttpRequest req, HttpResponse res) {
+  res.statusCode = 500;
+  return "Internal Server Error!";
+}
+
+// Missing Route handler
+FutureOr missingHandler(HttpRequest req, HttpResponse res) {
+  res.statusCode = 404;
+  return "This route doesn't exist!";
 }
